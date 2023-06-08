79e13198324d2a8dacef0fb821a7c224eccf90ef
subhanprime <subhan25902@gmail.com>
2023-06-08 16:06:35 +0500

    chages

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-    return res.status(200).json({ message: "this is server running" })
-})
-app.listen(PORT, _ => {
-    console.log('server running')
-})
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "this is server running" });
+});
+const connectDb = async () => {
+   console.log("connect function call")
+  const resp = await mongoose.connect("mongodb://mongo-db-service/podDb");
+  if (resp) console.log("db connected");
+  else{
+    console.log("db not connected")
+  }
+};
+app.listen(PORT, (_) => {
+  console.log("server running");
+  connectDb()
+});
